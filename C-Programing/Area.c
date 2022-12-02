55184f0ddafc2698ac99c1742fffce9b09dd2c64
mind bender <98180582+Tharun-DV@users.noreply.github.com>
2022-12-02 10:40:44 +0530

    Update Area.c

--- C-Programing/Area.c
+++ C-Programing/Area.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main(){
-    int a,b,h;
+    float a,b,h;
     printf("Enter Base (a) :");
-    scanf("%d",&a);
+    scanf("%f",&a);
     printf("Enter Base (b) :");
-    scanf("%d",&b);
+    scanf("%f",&b);
     printf("Enter Height (h) :");
-    scanf("%d",&h);
-    printf("The Area of Trapizium is (A) : %d",((a+b)/2)*h);
+    scanf("%f",&h);
+    printf("The Area of Trapizium is (A) : %.2f",((a+b)/2)*h);
 
 }
