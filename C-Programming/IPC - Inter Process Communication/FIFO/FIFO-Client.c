047f345d351987ef12006129002d2d854415bc65
mind bender <98180582+Tharun-DV@users.noreply.github.com>
2022-12-08 12:07:17 +0530

    Update FIFO-Client.c

--- C-Programming/IPC - Inter Process Communication/FIFO/FIFO-Client.c
+++ C-Programming/IPC - Inter Process Communication/FIFO/FIFO-Client.c
@@ -13,18 +13,18 @@ int main(){
     mkfifo(myfifo, 0666);
     char str1[80], str2[80];
     while (1){
-    // First open in read only and read
-    fd1 = open(myfifo,O_RDONLY);
-    read(fd1, str1, 80);
-    // Print the read string and close
-    printf("User1: %s\n", str1);
-    close(fd1);
-    // Now open in write mode and write
-    // string taken from user.
-    fd1 = open(myfifo,O_WRONLY);
-    fgets(str2, 80, stdin);
-    write(fd1, str2, strlen(str2)+1);
-    close(fd1);
+        // First open in read only and read
+        fd1 = open(myfifo,O_RDONLY);
+        read(fd1, str1, 80);
+        // Print the read string and close
+        printf("User1: %s\n", str1);
+        close(fd1);
+        // Now open in write mode and write
+        // string taken from user.
+        fd1 = open(myfifo,O_WRONLY);
+        fgets(str2, 80, stdin);
+        write(fd1, str2, strlen(str2)+1);
+        close(fd1);
     }
 return 0;
 }
